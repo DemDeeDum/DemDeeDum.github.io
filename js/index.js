c37cd830dbc688e65276d2bb4047c90ba69a7843
Dima <d1m.dee.dum@gmail.com>
2021-12-27 19:30:08 +0200

    Finish slider develop on index page

--- js/index.js
+++ js/index.js
@@ -7,19 +7,19 @@ $('.top-sells-item-container').slick({
     autoplaySpeed: 2000,
     speed: 1000,
     responsive: [{
-            breakpoint: 1200,
+            breakpoint: 1350,
             settings: {
                 slidesToShow: 3
             }
         },
         {
-            breakpoint: 1050,
+            breakpoint: 1100,
             settings: {
                 slidesToShow: 2
             }
         },
         {
-            breakpoint: 750,
+            breakpoint: 800,
             settings: {
                 slidesToShow: 1
             }
